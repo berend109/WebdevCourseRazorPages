ede7c86e5c4280b8a8ecf5ba8795fc21afa0cf02
berend109 <berend109@gmail.com>
2023-02-14 13:34:19 +0100

    added input from input and set value on hidden input

--- Exercises/Pages/Lesson1/Assignment3.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment3.cshtml.cs
@@ -1,12 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Exercises.Pages.Lesson1
 {
     public class Assignment3 : PageModel
     {
-        public void OnGet()
-        {
+        public int FirstInput { get; set; }
 
+       public void OnPost([FromForm] int input)
+        {
+            FirstInput = input;
         }
     }
 }
